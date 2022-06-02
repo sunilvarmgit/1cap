8d88f6468505fa612ad578ba5cd43f314ace99f8
Sunil Varma <sunil.c@beeceeyes.com>
2022-06-02 11:53:31 +0000

    UI App folder commmit

---
+++ app/risks/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.me.risks.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
